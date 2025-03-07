d61160c430c3ba951446613070351e1b5e6ee01e
Fernando Paredes Rios <fprios112@gmail.com>
2025-03-06 23:27:31 -0500

    update ads

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 // HomeScreen.tsx
 import React, { useState } from "react";
 // import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, View, Image, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, View, Image, Platform } from "react-native";
 import TicTacToeLogo from "../../assets/tic-tac-toe-logo.png";
 import { CommonButton } from "../components/CommonButton";
 import { PlayerNameModal } from "../components/PlayerNameModal";
@@ -13,10 +13,10 @@ import {
 import { StatusBar } from "expo-status-bar";
 
 export const HomeScreen = () => {
-  
-  const iOSadUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-7992487598629612~9539272308';
-  const androidadUnitId = __DEV__ ?  TestIds.BANNER: "ca-app-pub-7992487598629612~4094276459"
-  
+
+  const iOSadUnitId = 'ca-app-pub-7992487598629612~9539272308';
+  const androidadUnitId = __DEV__ ? TestIds.BANNER : "ca-app-pub-7992487598629612~4094276459"
+
   const [isModalVisible, setModalVisible] = useState<boolean>(false);
   const [mode, setMode] = useState<"onePlayer" | "twoPlayers">("onePlayer");
 
@@ -36,7 +36,7 @@ export const HomeScreen = () => {
         />
         <CommonButton
           onPress={() => handlePress("twoPlayers")}
-          text="Play with Other"
+          text="Play with a Friend"
         />
       </View>
       <PlayerNameModal
